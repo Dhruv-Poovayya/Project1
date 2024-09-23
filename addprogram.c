33661121c1560e65dc9272aa49a6b61c298e2e45
Dhruv Poovayya <dhruvpoovayya123@gmail.com>
2024-09-23 06:56:01 +0530

    Added Function

---
+++ addprogram.c
@@ -0,0 +1,13 @@
+#Function to add two numbers 
+void add_number()
+{
+	int a=0;
+	int b=0;
+	int sum=0;
+	printf("Enter the value of a and b");
+	scanf("%d %d ",&a &b);
+	sum=a+b;
+
+	printf("the sum is %d",sum);
+	
+}
